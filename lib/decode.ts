26b32cc4006845718b485ff427fa66977b26e9dc
Hasan <wilayudhah@gmail.com>
2026-05-23 17:13:41 +0700

    test menu random ads fix 1

--- lib/decode.ts
+++ lib/decode.ts
@@ -1,14 +1,27 @@
 export function decodePayload(
-  slug: string
+  payload: string
 ) {
 
   try {
 
-    return JSON.parse(
+    const bytes =
+      new Uint8Array(
 
-      atob(slug)
+        payload.match(/.{1,2}/g)
 
-    )
+          ?.map((hex) =>
+
+            parseInt(hex, 16)
+
+          ) || []
+
+      )
+
+    const json =
+      new TextDecoder()
+        .decode(bytes)
+
+    return JSON.parse(json)
 
   } catch {
 
