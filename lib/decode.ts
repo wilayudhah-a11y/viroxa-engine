83b0b5d41dfbb744370f280d7dd26957ef0e25b5
Hasan <wilayudhah@gmail.com>
2026-05-23 16:33:52 +0700

    test menu random ads fix

--- lib/decode.ts
+++ lib/decode.ts
@@ -1,38 +1,19 @@
-import {
-  decompressFromEncodedURIComponent,
-} from 'lz-string'
-
 export function decodePayload(
   slug: string
 ) {
+
   try {
-    const parts =
-  slug.split('~')
 
-const stripped =
-  parts.slice(1).join('~')
+    return JSON.parse(
 
-    const reversed =
-      stripped
-        .split('')
-        .reverse()
-        .join('')
-const unsafe =
-  reversed
-    .replace(/-/g, '+')
-    .replace(/_/g, '/')
-	
-    const decompressed =
-  decompressFromEncodedURIComponent(
-    unsafe
-  )
+      atob(slug)
 
-    if (!decompressed) {
-      return null
-    }
+    )
 
-    return JSON.parse(decompressed)
   } catch {
+
     return null
+
   }
+
 }
