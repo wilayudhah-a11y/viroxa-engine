14937537b2c4837c2ea00cb662de33ce69538342
Hasan <wilayudhah@gmail.com>
2026-06-02 10:10:12 +0700

    reports  today

--- app/realtime/page.tsx
+++ app/realtime/page.tsx
@@ -484,6 +484,98 @@ const filteredClicks =
         )
 
       }
+	  
+if(
+  reportPeriod ===
+  "last7days"
+){
+
+  const last7 =
+    new Date()
+
+  last7.setDate(
+    last7.getDate() - 7
+  )
+
+  return (
+    new Date(
+      click.created_at
+    ) >= last7
+  )
+
+}
+
+if(
+  reportPeriod ===
+  "thisweek"
+){
+
+  const now =
+    new Date()
+
+  const startOfWeek =
+    new Date(now)
+
+  startOfWeek.setDate(
+    now.getDate() -
+    now.getDay()
+  )
+
+  startOfWeek.setHours(
+    0,0,0,0
+  )
+
+  return (
+    new Date(
+      click.created_at
+    ) >= startOfWeek
+  )
+
+}
+
+if(
+  reportPeriod ===
+  "lastweek"
+){
+
+  const now =
+    new Date()
+
+  const startOfThisWeek =
+    new Date(now)
+
+  startOfThisWeek.setDate(
+    now.getDate() -
+    now.getDay()
+  )
+
+  startOfThisWeek.setHours(
+    0,0,0,0
+  )
+
+  const startOfLastWeek =
+    new Date(
+      startOfThisWeek
+    )
+
+  startOfLastWeek.setDate(
+    startOfLastWeek.getDate() - 7
+  )
+
+  return (
+
+    new Date(
+      click.created_at
+    ) >= startOfLastWeek &&
+
+    new Date(
+      click.created_at
+    ) < startOfThisWeek
+
+  )
+
+}
+	  
       return true
 
     }
